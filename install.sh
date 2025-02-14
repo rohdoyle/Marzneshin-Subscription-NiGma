bf9ef5b6237712e1fc11f58661d8d3ef63d3f79e
rohayar <136110068+rohdoyle@users.noreply.github.com>
2025-02-14 18:57:56 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 download_url="https://raw.githubusercontent.com/vUnkname/Marzban-Subscription-NiGma/main/index.html"
-template_dir="/var/lib/marzban/templates/subscription"
+template_dir="/var/lib/marzneshin/templates/subscription"
 template_file="$template_dir/index.html"
-env_file="/opt/marzban/.env"
+env_file="/etc/opt/marzneshin/.env"
 BRIGHT_GREEN="\e[92m"
 Cranberry="\e[38;2;158;16;66m"
 RESET="\e[0m"
@@ -30,11 +30,11 @@ update_admin_id() {
 apply_config() {
     echo "Applying configuration..."
     sleep 3
-    echo 'CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"' | sudo tee -a "$env_file"
+    echo 'CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzneshin/templates/"' | sudo tee -a "$env_file"
     echo 'SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"' | sudo tee -a "$env_file"
     echo "Restarting Marzban..."
     sleep 3
-    sudo marzban restart
+    sudo marzneshin restart
 }
 
 case $choice in
